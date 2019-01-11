3666c9ed67c6bf5fc7e2c443e3fcfb70b7720c5f
Shubham Dubey <sdubey504@gmail.com>
2019-01-11 12:10:37 +0530

    added https support

--- script.py
+++ script.py
@@ -1,66 +1,55 @@
-import sys
-import socket
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import urllib3
+#!/usr/bin/python
 import certifi
-try:
-    from urllib.parse import urlsplit
-except ImportError:
-     from urlparse import urlsplit
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import logging
+import socket
+import sys
+import urllib3
+from urlparse import urlsplit
 
-
+#reading file passed through argument
 def readFile(filename):
 	try:
-		url_list = open(filename, 'r')
-		return url_list
+		urlList = open(filename, 'r')
+		return urlList
 	except IOError:
 		print("Could not read file:", filename)
 		sys.exit()
+	return
 
-
+#scanning for .git exposed happening here
 def scanForVuln(url):
 
-	#lets remove http/https for now, we will add it below
-	url.replace("http://","")
-	url.replace("https://","")
-
-    #simple check if url/ip is valid or not
-    try:
-			ip = socket.gethostbyname(url)
-			s = socket.socket()
-			s.connect((ip,GIT_PORT))
-
-		except socket.gaierror as error:
-			return
-
-
 	#expecting .git/HEAD will be readble if the website is under git directory
-    fullUrlSecure = "https://{0}/.git/HEAD".format(url)
-	fullUrl = "http://{0}/.git/HEAD".format(url)
-    agent = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'}
+	fullUrl = "{0}/.git/HEAD".format(url[:-1])
+	agent = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'}
 
-    httpsRequest = urllib3.PoolManager(
-        cert_reqs='CERT_REQUIRED',
-        ca_certs=certifi.where()
-    )
-	httpRequest = urllib3.PoolManager()
-    timeout = urllib3.Timeout(connect=5.0, read=10.0)
+	httpsRequest = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
+	#print(httpsRequest)
+	timeout = urllib3.Timeout(connect=5.0, read=10.0)
+	try:
+		httpsResponse = httpsRequest.request('GET', fullUrl, headers=agent, timeout=timeout)
+	except e as Error:
+		print e
 
-    r = httpRequest.request('GET', furl, headers=agent, timeout=timeout)
-    print("TEST: {0}".format(furl))
-    print type(r.data)
-    a = r.data
-    print a[0:10]
-    if (a == "ref: refs/heads/master\n"):
-        print "Git is visible"
-    else:
-        print "Git is not visible"
-    r = None
+	print("TESTING: {0}".format(fullUrl))
+	if (httpsResponse.data == "ref: refs/heads/master\n"):
+		print ".git is Exposed for url {0}".format(url)
+		print "---------------------------"
+	else:
+		print ".git is secure or not present\n"
+		print "---------------------------"
+		httpsResponse = None
+    	return
 
+	httpsResponse = None
+	return
 
+#using concurrency for huge input
 def findGitHosting(urlList):
-	with ThreadPoolExecutor(max_workers=100) as executor:
-	          results = executor.map(scanForVuln, urlList)
+    with ThreadPoolExecutor(max_workers=100) as executor:
+		results = executor.map(scanForVuln, urlList)
+
 
 
 def main():
@@ -69,5 +58,10 @@ def main():
 		return
 	else:
 		filename = sys.argv[1]
-		urlList = read_file(filename)
-		scanForVuln(urlList)
+		urlList = readFile(filename)
+		print ""
+		findGitHosting(urlList)
+		exit()
+
+if __name__== "__main__":
+    main()
